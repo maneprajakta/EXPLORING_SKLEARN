b55ff0c48b93dff125989744a76b0c6d82f2aed3
Prajakta Mane <prajakta916mane1@gmail.com>
2020-10-25 22:27:18 +0530

    day21

---
+++ day21/anomalie.py
@@ -0,0 +1,5 @@
+'''
+Anomaly detection is a technique used to identify data points in dataset that does not fit well with the rest of the data. 
+'''
+
+#Two methods namely outlier detection and novelty detection can be used for anomaly detection.
